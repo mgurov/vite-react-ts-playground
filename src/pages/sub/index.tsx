cf16263eb9516e160a0629d57c4b2ad32bed41e0
Mykola Gurov <ngurov@gmail.com>
2024-08-24 13:22:41 +0200

    goood

--- src/pages/sub/index.tsx
+++ src/pages/sub/index.tsx
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return (<h1 data-testid="routed">sub</h1>)
+    return (<>
+        <h1 data-testid="routed">sub</h1>
+        <Link to="/sub/nice">nice</Link>
+    </>)
 }
